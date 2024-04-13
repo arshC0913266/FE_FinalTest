87166365e89823f16cca43739a2ca53b66e02e9c
arshC0913266 <166955700+arshC0913266@users.noreply.github.com>
2024-04-13 17:58:55 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ function generateTable() {
     for (var i = 0; i < rows; i++) {
         table += '<tr>';
         for (var j = 0; j < cols; j++) {
-            table += '<td>&nbsp;</td>';
+            table += '<td style="color: white;">row: ' + (i + 1) + ', column: ' + (j + 1) + '</td>';
         }
         table += '</tr>';
     }
@@ -24,23 +24,49 @@ function generateTable() {
     table += '</table>';
 
     document.getElementById('tableContainer').innerHTML = table;
-    document.getElementById('inputValues').innerHTML = 'Number of rows: ' + rows + ', Number of columns: ' + cols;
 
     // Hide the "Generate Table" button
     document.getElementById('b1').style.display = 'none';
+    document.getElementById('t1').style.display = 'none';
+    document.getElementById('t2').style.display = 'none';    
+    document.getElementById('tableContainer').style.color = 'black !important';
+    document.body.style.color = "white";
+      var cells = document.querySelectorAll("td");
+
+      //   cell color
+      cells.forEach(function (cell) {
+        cell.style.color = "black";
+      });
+
+      var h2Tags = document.getElementsByTagName("h2");
+
+      for (var i = 0; i < h2Tags.length; i++) {
+        h2Tags[i].style.color = "black";
+      }
+      document.getElementsByTagName("font")[0].style.display = "none";
 }
 
 function addColumn() {
     var tableRows = document.querySelectorAll('#tableContainer table tr');
     for (var i = 0; i < tableRows.length; i++) {
         var cell = document.createElement('td');
-        cell.innerHTML = '&nbsp;';
+        cell.innerHTML = 'row: ' + (i + 1) + ', column: ' + (tableRows[i].cells.length + 1);
+        cell.style.color = 'black'; // Set text color to black
         tableRows[i].appendChild(cell);
     }
+    document.body.style.color = "white";
+      var cells = document.querySelectorAll("td");
 
-    var cols = document.querySelectorAll('#tableContainer table tr:first-child td').length;
-    var rows = document.querySelectorAll('#tableContainer table tr').length;
-    document.getElementById('inputValues').innerHTML = 'Number of rows: ' + rows + ', Number of columns: ' + cols;
+      //   cell color
+      cells.forEach(function (cell) {
+        cell.style.color = "black";
+      });
+
+      var h2Tags = document.getElementsByTagName("h2");
+
+      for (var i = 0; i < h2Tags.length; i++) {
+        h2Tags[i].style.color = "black";
+      }
 }
 
 function addRow() {
@@ -49,10 +75,20 @@ function addRow() {
     var newRow = table.insertRow();
     for (var i = 0; i < cols; i++) {
         var cell = newRow.insertCell();
-        cell.innerHTML = '&nbsp;';
+        cell.innerHTML = 'row: ' + (table.rows.length) + ', column: ' + (i + 1);
+        cell.style.color = 'black'; // Set text color to black
     }
+    document.body.style.color = "white";
+      var cells = document.querySelectorAll("td");
 
-    var cols = document.querySelectorAll('#tableContainer table tr:first-child td').length;
-    var rows = document.querySelectorAll('#tableContainer table tr').length;
-    document.getElementById('inputValues').innerHTML = 'Number of rows: ' + rows + ', Number of columns: ' + cols;
+      //   cell color
+      cells.forEach(function (cell) {
+        cell.style.color = "black";
+      });
+
+      var h2Tags = document.getElementsByTagName("h2");
+
+      for (var i = 0; i < h2Tags.length; i++) {
+        h2Tags[i].style.color = "black";
+      }
 }
